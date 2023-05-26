0cd2b8f3b5ccf810c18cf94440e9508a9742c853
Ahmedhaggg <ahmedhaggagrady@gmail.com>
2023-05-26 09:22:12 +0300

    modify 404 page because deploying error

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,17 +1,10 @@
-import { Box, Alert, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import NextLink from "next/link";
 import { Link } from "@mui/material";
-import { useRouter } from "next/router";
 import React from "react";
 import errorImage from "../assets/404-pages.jpg";
 
 export default function NotFound() {
-    // let router = useRouter();
-    // let page = router.asPath.split("/")[1];
-
-    return <>
-        <img src={errorImage.src} alt="notfound" width="100%" height="100%" />
-    </>;
 
     return (
         <Box width="100%" height="100%" sx={{ textDecoration: "none" }}>
