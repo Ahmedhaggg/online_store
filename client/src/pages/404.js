682a66034e3ce4d92f2b464bb597924e8e80bc31
Ahmedhaggg <ahmedhaggagrady@gmail.com>
2023-05-27 09:27:24 +0300

    fix error in 404 page

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,17 +1,20 @@
 import { Box } from "@mui/material";
 import NextLink from "next/link";
 import { Link } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import errorImage from "../assets/404-pages.jpg";
-
+import { useRouter } from "next/router";
 export default function NotFound() {
-
+    // let [visitor, setVisitor] = useState("user")
+    let router = useRouter()
+    let visitor = router.asPath.split("/")[1]
+    
     return (
         <Box width="100%" height="100%" sx={{ textDecoration: "none" }}>
             <img src={errorImage.src} width="100%" height="100%" />
-            <NextLink href={page !== "admin" ? "/" : "/admin"}>
+            <NextLink href={visitor !== "admin" ? "/" : "/admin"}>
                 <Box sx={{ textAlign: "center"}}>
-                    <Link href={page !== "admin" ? "/" : "/admin"}>Home</Link>
+                    <Link href={visitor !== "admin" ? "/" : "/admin"}>Home</Link>
                 </Box>
             </NextLink>
         </Box>
